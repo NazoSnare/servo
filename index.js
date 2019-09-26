055cf1e3f91f262976b3cc021f539784bb820608
Malcom <mm@hhpty.com>
2019-09-26 12:04:06 +0200

    script ready for testing

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 const WebSocket = require('ws');
 const path = require('path');
-const {spawn} = require('child_process')
+const util = require("util");
+const { spawn } = require('child_process')
 
 const ws = new WebSocket("ws://10.112.20.171:8080");
 
 ws.on('open', function open() {
     ws.send(JSON.stringify({
-        type:"machine:connected",
+        type: "machine:connected",
         data: {
-           machine:"liquid-studio"
+            machine: "liquid-studio"
         }
     }));
     console.log('opened');
-  });
-  
-  ws.on('message', (data) =>{
-      let receivedData = JSON.parse(data).data;
-      let messageType = JSON.parse(data).type;
+});
 
-      if (messageType === "user:redeemed") {
+ws.on('message', (data) => {
+    let receivedData = JSON.parse(data).data;
+    let messageType = JSON.parse(data).type;
+
+    if (messageType === "user:redeemed") {
         console.log("user redeemed", receivedData);
         //spaw reece serve
-        let reece = spawn('python smartvent-kyc.py',[]);
-      }
+        let process = spawn('python', ['smartvent-kyc.py']);
 
-  });
+
+        process.stdout.on('data', function (chunk) {
+
+            var textChunk = chunk.toString('utf8');// buffer to string
+
+            util.log(textChunk);
+        });
+    }
+
+});
